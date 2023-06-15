3f06d2b3aeafa272bb81b1e402b80b019a54a63f
TSI-sangramdesai <sangramsinh.desai@t-systems.com>
2023-06-15 18:08:50 +0530

    files app test cases updated

--- magentacloud25/cypress/e2e/left-menu-spec.cy.js
+++ magentacloud25/cypress/e2e/left-menu-spec.cy.js
@@ -1,38 +1,26 @@
-describe('Left menu related changes', () => {
+describe('Themes related changes', () => {
   beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
     cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
-    cy.get('.grouptop input').type(`${Cypress.env('local').user}{enter}`)
-    cy.get('.groupbottom input').type(`${Cypress.env('local').password}{enter}`)
+    cy.get('.input-field__main-wrapper #user').type(`${Cypress.env('local').user}{enter}`)
+    cy.get('.input-field__main-wrapper #password').type(`${Cypress.env('local').password}{enter}`)
   })
-
-  it('Check Css for delete activity button', () => {
-    cy.get('.app-navigation-translation').should('have.css', 'top', '73px')
-    cy.get('.NextCloudPorgressBar').find('.customprogressbar').should('have.css', 'height', '4px').should('have.css', 'border-radius', '4px').should('have.css', 'margin-bottom', '8px')
-    cy.get('.memory-ocup-message').should('have.css', 'font-size', '14px')
-    cy.get('#app-navigation').find('.custom-button').find('.btn-style').should('have.css', 'font-size', '16px')
-    .should('have.css', 'cursor', 'pointer').should('have.css', 'font-weight', '400')
-    .should('have.css', 'display', 'inline-block').should('have.css', 'font-family', 'TeleNeoWeb, TeleNeo, sans-serif')
-    cy.get('#app-settings-header').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
-    cy.get('#app-settings-header').find('.settings-button').should('have.css', 'display', 'block').should('have.css', 'height', '20px').should('have.css', 'width', '287px')
-    .should('have.css', 'background-repeat', 'no-repeat').should('have.css', 'box-shadow', 'none').should('have.css', 'border', '0px none rgb(226, 0, 116)')
-    .should('have.css', 'border-radius', '0px').should('have.css', 'text-align', 'left').should('have.css', 'padding-left', '40px')
-    .should('have.css', 'font-weight', '400')
-  })
-
 
   it('Left menu names change in english', () => {
     cy.wait(3000)
-    cy.get('.settingsdiv').click()
+    cy.get('#user-menu').click()
     cy.wait(1000)
-    cy.get('a[href*="/index.php/settings/user"]').contains('Einstellungen').click()
+    cy.get('#settings.menu-entry').click()
     cy.wait(2000)
-    cy.get('#languageinput').select('English')
+    cy.get('#account-setting-language').select('English (US)')
     cy.wait(2000)
     cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
     cy.wait(1000)
-    cy.get("li[data-id='nmc_files_activity'] a").should('have.contain','Activities')
-    cy.get("#app-navigation li[data-id='favorites'] a").should('have.contain','Favourites')
-    cy.get("#app-navigation li[data-id='files'] a").should('have.contain','All files')
+    cy.get("#app-navigation li[data-id='favorites'] a.nav-icon-favorites.svg").should('have.contain','Favorites')
+    cy.get("#app-navigation li[data-id='files'] a.nav-icon-files.svg").should('have.contain','All files')
     cy.get("#app-navigation li[data-id='sharingout'] a").should('have.contain','My shares')
     cy.get("#app-navigation li[data-id='sharingin'] a").should('have.contain','Shared with me')
     cy.get("#app-navigation li[data-id='trashbin'] a").should('have.contain','Deleted files')
@@ -44,17 +32,15 @@ describe('Left menu related changes', () => {
 
   it('Left menu names - in German', () => {
     cy.wait(3000)
-    cy.get('.settingsdiv').click()
+    cy.get('#user-menu').click()
     cy.wait(1000)
-    cy.get('a[href*="/index.php/settings/user"]').contains('Settings').click()
+    cy.get('#settings.menu-entry').click()
     cy.wait(2000)
-    cy.get('#languageinput').select('Deutsch')
+    cy.get('#account-setting-language').select('Deutsch (Förmlich: Sie)')
     cy.wait(2000)
     cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
     cy.wait(1000)
 
-    cy.get("li[data-id='nmc_files_activity'] a").should('have.contain','Aktivitäten')
-    cy.get("#app-navigation li[data-id='favorites'] a").should('have.contain','Favoriten')
     cy.get("#app-navigation li[data-id='files'] a").should('have.contain','Alle Dateien')
     cy.get("#app-navigation li[data-id='sharingout'] a").should('have.contain','Meine geteilten Inhalte')
     cy.get("#app-navigation li[data-id='sharingin'] a").should('have.contain','Mit mir geteilt')
@@ -62,10 +48,8 @@ describe('Left menu related changes', () => {
     cy.get('.custom-button .btn-default').should('have.contain','Speicherplatz erweitern')
     cy.get('.settings-button').should('have.contain','Anzeigeeinstellungen')
 
-    cy.get("label[for='showhiddenfilesToggle']").should('have.contain','Versteckte Dateien anzeigen')
-    cy.get("label[for='cropimagepreviewsToggle']").should('have.contain','Vorschaubilder  beschneiden')
+    cy.get("#files-setting-showhidden label[for='showhiddenfilesToggle']").should('have.contain','Versteckte Dateien anzeigen')
   })
-
 })
 
 
