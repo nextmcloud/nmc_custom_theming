b62b0c9c3df8b105f4bbca8f8c48711675392dfc
TSI-sangramdesai <sangramsinh.desai@t-systems.com>
2023-06-30 18:01:20 +0530

    secondary Email confirmation and success pages css changes

--- magentacloud25/core/templates/success.php
+++ magentacloud25/core/templates/success.php
@@ -7,7 +7,7 @@
 <div class="update">
 	<h2><?php p($_['title']) ?></h2>
 	<p> <?php p($l->t('Your email address has been successfully confirmed and is now in use.')); ?></p>
-	<p><a class="button primary" href="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkTo('', 'index.php')) ?>">
+	<p><a class="button primary success-btn" href="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkTo('', 'index.php')) ?>">
 		<?php p($l->t('Go to %s', [$theme->getName()])); ?>
 	</a></p>
 </div>
