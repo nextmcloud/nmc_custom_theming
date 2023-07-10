a4cc4b15faf3d7a6927a925d2ecf2329de1e25ba
DanielEngelschalk <daniel.engelschalk@gitcom.cloud>
2023-07-10 15:46:54 +0200

    Javascript code change for tealium

--- nextmagentacloud21/core/js/customutils.js
+++ nextmagentacloud21/core/js/customutils.js
@@ -612,52 +612,15 @@ function leftSidebarEvents() {
 }
 
 // Tealium
-function consent_layer_events() {
-  document.addEventListener("click", function (e) {
-    if (e.target.closest("#consentAcceptAll") || e.target.closest("#rejectAll") || e.target.closest("#consentSettingsAcceptAll") || e.target.closest("#consentAcceptChoice")) {
-      consent_layer_events();
-    }
-  })
-
-  // tracking events
-  if (typeof utag !== 'undefined' && utag.view()) {
-    var utag_data = {
-      page_content_id: "magentacloud.de.privatkunden.consentlayer",
-      page_type: "theme"
-    }
-    utag.view(utag_data);
-  }
-
-  // tealium action
-  const locSearch = window.location.search;
-  const urlSearchParams = new URLSearchParams(locSearch);
-
-  if (locSearch) { // all logins except telekom login
-    if (urlSearchParams.get('redirect_url')) {
-      telekom_redirection();
-    } else {
-      if (!urlSearchParams.get('direct')) {
-        document.getElementsByClassName('login-section')[0].style.visibility = 'visible';
-      }
-    }
-  } else { // telekom login redirection
-    telekom_redirection();
+window.addEventListener("consentChanged", ()=> redirectToTelekomLogin());
+window.onload = function () {
+  if( "object" === typeof utag &&
+    "object" === typeof utag.gdpr &&
+    utag.gdpr.getConsentState() !== 0) {
+    redirectToTelekomLogin();
   }
 }
 
-function telekom_redirection() {
-  const telLoginButton = document.getElementById('alternative-logins');
-  if (telLoginButton && consentCookies()) {
-    telLoginButton?.children[0]?.click();
-  } else {
-    const loginSect = document.getElementsByClassName('login-section');
-    if (loginSect && consentCookies()) {
-      loginSect[0].style.visibility = 'visible';
-    }
-  }
-}
-
-function consentCookies() {
-  const constentCookieValue = document.cookie.match(/^(.*;)?\s*CONSENTMGR\s*=\s*[^;]+(.*)?$/);
-  return constentCookieValue && constentCookieValue[0].includes("consent");
+function redirectToTelekomLogin(){
+  window.location.href = "/apps/user_oidc/login/1";
 }
